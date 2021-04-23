1f609a40f3b3746442e97493992475a88bbb8dc0
Kotomyra Uchida <kotomyrau@gmail.com>
2021-04-23 11:24:26 +0100

    mailchimp css

--- src/components/subscribePage/index.js
+++ src/components/subscribePage/index.js
@@ -1,16 +1,45 @@
-import React from 'react'
+import React, { Component } from 'react'
 import "./subscribe.css"
 import MailchimpSubscribe from "react-mailchimp-subscribe"
+import Mailchimp from 'react-mailchimp-form'
+// const Subscribe = () => {
+//     return (
+//         <div>
+//               <MailchimpSubscribe url={process.env.REACT_APP_MAILCHIMP_URL} />
 
-const Subscribe = () => {
+//         </div>
+//     )
+// }
+ 
+class Subscribe extends Component {
+  render() {
     return (
-        <div>
-              <MailchimpSubscribe url={process.env.REACT_APP_MAILCHIMP_URL} />
-
-        </div>
+      <Mailchimp
+        action= {process.env.REACT_APP_MAILCHIMP_URL}
+          fields={[
+            {
+              name: 'EMAIL',
+              placeholder: 'Email',
+              type: 'email',
+              required: true,
+            }
+          ]}
+          messages = { 
+            {
+              sending: "Kleaning...",
+              success: "Thank you for subscribing!",
+              error: "An unexpected internal error has occurred.",
+              empty: "Please enter a valid email.",
+              duplicate: "Too many subscribe attempts for this email address",
+              button: "ScrubScribe"
+            }
+          }
+      />
     )
+  }
 }
+   
+  export default Subscribe;
 
 
-
-export default Subscribe;
+// export default Subscribe;
