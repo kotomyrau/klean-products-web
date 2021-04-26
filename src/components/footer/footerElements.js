350d8059753fc510e5d7605e0b122f052f98c8f8
Kotomyra Uchida <kotomyrau@gmail.com>
2021-04-26 12:23:40 +0100

    footer* added

--- src/components/footer/footerElements.js
+++ src/components/footer/footerElements.js
@@ -1,18 +1,33 @@
 import styled from 'styled-components'
 
 export const FooterContainer = styled.div`
-  background-color: blue;
-  height: 10vh;
+  background-color: #D4A373;
+  height: auto;
+  display: grid; 
+  grid-template-areas: 
+    "brand newsletter socialMedia";
+  grid-template-columns:0.5fr 1fr 0.5fr;
+  grid-template-rows: auto;
+  padding: 4vh 2vw 0;
 `
 export const FooterCopyrightWrapper = styled.div`
-  bottom: 0;
+  grid-area: socialMedia;
 `
-export const FooterCopyrightText = styled.p`
+export const FooterCopyrightText = styled.h6`
   text-align: right;
+`
+export const FooterBrandWrapper = styled.div`
+  grid-area: brand;
+`
+export const BrandName = styled.h6`
+
+`
+export const BrandSlogan = styled.p`
+
 `
 
 export const SocialMediaWrapper = styled.div`
-
+  grid-area: newsletter;
 `
 export const SocialMediaHeader = styled.h6`
 
@@ -24,4 +39,12 @@ export const SocialMediaItem = styled.div`
 export const SocialMediaLink = styled.div`
 
 `
+
+export const SubscribeWrapper = styled.div`
+
+`
+export const SubscribeHeader = styled.h6`
+
+`
+
 
--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,33 +1,66 @@
 import React from 'react'
-import {FooterContainer, SocialMediaWrapper, SocialMediaHeader, SocialMediaItem, SocialMediaLink, FooterCopyrightWrapper, FooterCopyrightText} from './footerElements'
+import {FooterContainer, FooterBrandWrapper, BrandName, BrandSlogan, SocialMediaWrapper, SocialMediaHeader, SocialMediaItem, SocialMediaLink, SubscribeWrapper, SubscribeHeader, FooterCopyrightWrapper, FooterCopyrightText} from './footerElements'
 import { FaInstagram, FaFacebook, FaTwitter } from 'react-icons/fa';
+import Mailchimp from 'react-mailchimp-form'
 
 const Footer = () => {
-    return (
-        <>
-          <FooterContainer>
-          <SocialMediaWrapper>
-            <SocialMediaHeader>social media</SocialMediaHeader>
-            <SocialMediaItem>
-              <SocialMediaLink href="https://www.instagram.com/kleansoap/" target='blank'><FaInstagram /></SocialMediaLink>
-              </SocialMediaItem>
-              <SocialMediaItem>
-                <SocialMediaLink href="#" target='blank'><FaTwitter /></SocialMediaLink>
-              </SocialMediaItem>
-              <SocialMediaItem>
-                <SocialMediaLink href="#" target='blank'><FaFacebook /></SocialMediaLink>
-              </SocialMediaItem>
-          </SocialMediaWrapper>
+  return (
+    <>
+      <FooterContainer>
+            <FooterBrandWrapper>
+              <BrandName>
+              Klean Products
+              </BrandName>
+              <BrandSlogan>
+              Stay Klean, feel klean
+              </BrandSlogan>
+            </FooterBrandWrapper>
+          <SubscribeWrapper>
+
+            <SubscribeHeader>Stay up to date</SubscribeHeader>
+
+            <Mailchimp
+              action= {process.env.REACT_APP_MAILCHIMP_URL}
+              fields={[
+                {
+                  name: 'EMAIL',
+                  placeholder: 'Email',
+                  type: 'email',
+                  required: true,
+                }
+              ]}
+              messages = { 
+                {
+                  sending: "Kleaning...",
+                  success: "Thank you for subscribing!",
+                  error: "An unexpected internal error has occurred.",
+                  empty: "Please enter a valid email.",
+                  duplicate: "Too many subscribe attempts for this email address",
+                  button: "ScrubScribe"
+                }
+              }
+              />
+            </SubscribeWrapper>
             <FooterCopyrightWrapper>
+                  <SocialMediaWrapper>
+                    <SocialMediaHeader>social media</SocialMediaHeader>
+                    <SocialMediaItem>
+                      <SocialMediaLink href="https://www.instagram.com/kleansoap/" target='blank'><FaInstagram /></SocialMediaLink>
+                      </SocialMediaItem>
+                      <SocialMediaItem>
+                        <SocialMediaLink href="#" target='blank'><FaTwitter /></SocialMediaLink>
+                      </SocialMediaItem>
+                      <SocialMediaItem>
+                        <SocialMediaLink href="#" target='blank'><FaFacebook /></SocialMediaLink>
+                      </SocialMediaItem>
+                  </SocialMediaWrapper>
               <FooterCopyrightText> 
                 Made with ♥︎ by Kotomaira Uchida
               </FooterCopyrightText> 
             </FooterCopyrightWrapper>
-          </FooterContainer>
-            
-        </>
-        
-    )
+          </FooterContainer>   
+    </>  
+  )
 }
 
 export default Footer
